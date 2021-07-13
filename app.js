5d7ad440ad939da9e2fa1391bc64e82a5580cd4f
Jay <limjunsung@gmail.com>
2021-07-13 12:42:04 +0900

    changed

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("IT WORKED")
 })
 
+app.get("/new", (req, res) => {
+  res.send("New also worked")
+})
+
 app.listen(process.env.PORT || 5000)
 
 module.exports = app
